efe03a508b3458d42e5555d9effb146473a22d66
Tzu Yin <tzyn.wang@gmail.com>
2022-08-11 20:45:37 +0800

    [feat] update src/index

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import App from '@Components/App'
-import ErrorBoundary from '@Components/Common/ErrorBoundary'
-
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '@Components/App';
+import AuthLayer from '@Components/Layer/Auth';
+import ErrorBoundary from '@Components/Common/ErrorBoundary';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <AuthLayer>
+        <App />
+      </AuthLayer>
     </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
